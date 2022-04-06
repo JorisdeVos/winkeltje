1a25fed945814593ee95e6f32c0c367d25d8a4aa
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-04-06 14:17:23 +0200

    detail page save 1

---
+++ details.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/pluk.css">
+	<title>Document</title>
+</head>
+<body>
+	<main>
+		<div class="topImage">
+			<p>Product Lorem.</p>
+		</div>
+		<div class="detailText">	
+			<div class="wrapper">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nulla, aliquam. Quas ducimus sequi officiis eos illum eaque, voluptatem, voluptatibus dolor distinctio nulla, maiores quam, iste nostrum incidunt beatae? Atque.
+				<img src="img/schep.png" alt="">
+			</div>
+		</div>
+		<div class="detailText2">
+			<p>Lorem, ipsum dolor, sit amet consectetur adipisicing elit. Quidem assumenda debitis unde iusto culpa quae soluta, dignissimos vel corrupti iure repudiandae delectus, exercitationem illo nesciunt necessitatibus ipsam provident minima sint?</p>
+		</div>
+	</main>
+</body>
+</html>
