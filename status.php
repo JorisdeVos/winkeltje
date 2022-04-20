5935c44cbd631a7ab79a824834cd72012485abea
calthan leander <calsonleander99@gmail.com>
2022-04-20 13:43:40 +0200

    added start of status page

---
+++ status.php
@@ -0,0 +1,49 @@
+<?php session_start()?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/pluk.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php require_once 'header.php';
+    $query = "SELECT * FROM  WHERE";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $todo = $statement->fetchAll(PDO::FETCH_ASSOC);
+  ?>
+  <main>
+  <table>
+    <thead>
+            <tr class="solid">
+                <th>Titel</th>
+        <th>Naam</th>
+        <th>Asignee</th>
+                <th>Bericht</th>
+                <th>Afdeling</th>
+                <th>deadline</th>
+                <th>Gemaakt op</th>
+                <th>Aanpassen</th>
+            </tr>
+          </thead>
+          <?php foreach($todo as $taken): ?>
+          <tbody>			                <tr>
+                  <?php $id = $taken['id'] ?>
+                  <td><?php echo $taken['titel']; ?></td>
+        <td><?php echo $taken['naam']; ?></td>
+        <td><?php echo $taken['asignee']; ?></td>
+                  <td><?php echo $taken['beschrijving']; ?></td>
+                  <td><?php echo $taken['afdeling']; ?></td>
+                  <td><?php echo $taken['deadline']; ?></td>
+                  <td><?php echo $taken['created_at']; ?></td>
+                  <td><a href="edit.php?id=<?php echo $id;?>">Aanpassen</a></td>
+              </tr>
+          </tbody>
+          <?php endforeach; ?>
+      </table>
+  </main>
+</body>
