c63ba8a2eca3af47cd0f285e5b934540dbb79a7e
calthan leander <calsonleander99@gmail.com>
2022-04-20 13:58:04 +0200

    completed delete function

--- backend/delete.php
+++ backend/delete.php
@@ -7,8 +7,10 @@ $mand = $_SESSION['basket'];
 $mandAmount = $_SESSION['amount'];
 $id = $_GET['itemId'];
 
-array_splice($mandAmount, $id, $id);
-array_splice($mand, $id, $id);
+array_splice($mandAmount, $id, 1);
+array_splice($mand, $id, 1);
 
+$_SESSION['basket'] = $mand;
+$_SESSION['amount'] = $mandAmount;
 header("location: ../winkelmand.php");
 ?>
